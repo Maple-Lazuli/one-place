ee4e59a4a9a12ec10240425a87d4afe6c870e599
Maple-Lazuli <ada.lazuli@spaceforce.mil>
2021-12-29 20:54:30 -0500

    implementing pages

--- one-place-web/src/components/projectPanel/ProjectBar.js
+++ one-place-web/src/components/projectPanel/ProjectBar.js
@@ -1,12 +1,21 @@
 import React from "react";
 import 'semantic-ui-css/semantic.min.css';
+import '../projects/ProjectOverlay.css'
 class ProjectBar extends React.Component {
+
 
     render() {
         return (
             <div className="ui visible inverted left vertical sidebar menu">
-                <div className="ui medium yellow header">{this.props.currentProject['Title']}</div>
-            </div>
+                <div>
+                    <div className="ui medium yellow header" onDoubleClick={this.toggleAddProjectsDisplay}>
+                        Open: {this.props.currentProject['title']}
+                    </div>
+                    <div>
+                        {/* Code to render all of the pages */}
+                    </div>
+                </div>
+            </div >
         )
     }
 }
